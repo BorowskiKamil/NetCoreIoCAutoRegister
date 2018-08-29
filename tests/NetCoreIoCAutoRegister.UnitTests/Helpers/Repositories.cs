d4d15cf37e76dacca240718878d6a89872d0a38e
Kamil Borowski <kb.kamil.borowski@gmail.com>
2018-08-29 21:28:23 +0200

    tests

---
+++ tests/NetCoreIoCAutoRegister.UnitTests/Helpers/Repositories.cs
@@ -0,0 +1,16 @@
+namespace NetCoreIoCAutoRegister.UnitTests.Helpers
+{
+	
+	public class FirstRepository : IFirstRepository { }
+	public interface IFirstRepository { }
+
+	public class SecondRepository : ISecondRepository { }
+	public interface ISecondRepository { }
+
+	public class ThirdRepository : IThirdRepository { }
+	public interface IThirdRepository { }
+
+	public class FourthRepository : IFourthRepository { }
+	public interface IFourthRepository { }
+
+}
--- tests/NetCoreIoCAutoRegister.UnitTests/ServiceCollectionTests.cs
+++ tests/NetCoreIoCAutoRegister.UnitTests/ServiceCollectionTests.cs
@@ -1,14 +1,43 @@
 using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreIoCAutoRegister.UnitTests.Helpers;
 using Xunit;
+using System.Linq;
 
 namespace NetCoreIoCAutoRegister.UnitTests
 {
     public class ServiceCollectionTests
     {
-        [Fact]
-        public void Test1()
-        {
 
+        private IServiceCollection _serviceCollection;
+
+        public ServiceCollectionTests() 
+        {
+            _serviceCollection = new ServiceCollection();
+        }
+
+        [Fact]
+        public void Should_Register_Three_Services()
+        {
+            _serviceCollection.RegisterTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.Name.EndsWith("Repository"))
+                .Except<IFourthRepository>()
+                .AsScoped();
+
+            Assert.Equal(3, _serviceCollection.Count());
+        }
+
+        [Fact]
+        public void Should_Register_Specified_Service()
+        {
+            _serviceCollection.RegisterTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.Name.EndsWith("Repository"))
+                .AsSingleton();
+
+            var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            Assert.IsType<FirstRepository>(serviceProvider.GetService<IFirstRepository>());
         }
     }
 }
